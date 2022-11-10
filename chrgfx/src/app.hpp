441e2593ae48db6177b0c0cf7fd3b09f0f3b6675
Damian R <damian@sudden-desu.net>
2022-11-11 00:35:27 +0900

    Corrected issue with excess tile row being improperly rendered when not using borders

--- chrgfx/src/app.hpp
+++ chrgfx/src/app.hpp
@@ -8,8 +8,8 @@ namespace APP
 {
 static unsigned int const VERSION_MAJOR {2};
 static unsigned int const VERSION_MINOR {1};
-static unsigned int const VERSION_PATCH {0};
-static char const * VERSION {"2.1.0"};
+static unsigned int const VERSION_PATCH {1};
+static char const * VERSION {"2.1.1"};
 
 static wchar_t const * NAME {L"chrgfx"};
 static wchar_t const * CONTACT {L"Damian R (damian@motoi.pro)"};
